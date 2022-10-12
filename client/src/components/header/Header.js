4187173f6a38fda2e1b195965277aa571efe217f
ManishaKoranga <korangamanisha07@gmail.com>
2022-10-12 17:24:02 +0530

    toggleswitch

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import "./Header.css"
 import {useThemeContext} from "../../theme/ThemeProvider"
-
+import ReactSwitch from "react-switch";
 export const Header = () => {
   const { theme, changeTheme } = useThemeContext();
 
   const handleNavbarClick = () => {
     changeTheme(theme === "light" ? "dark" : "light");
   }
-  
   return (
     <>
         <div className="navbar-container">
             <div className="navbar-title">Strings</div>
             <div className="navbar-links">
                 <ul>
-                    <li onClick={handleNavbarClick}>Dark Mode Toggle</li>
+                   <li> <div className="switch">
+                   <label> {theme === "light" ? "" : ""}</label>
+                   <ReactSwitch onChange={handleNavbarClick} checked={theme === "dark"} />
+        </div></li>
                     <li>Profile</li>
                 </ul>
             </div>
