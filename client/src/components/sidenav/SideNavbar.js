ca6b3f9fed0cc83c8ca83db3be137545f1edee98
Tanmayee3012 <tanupgosavi30@gmail.com>
2022-10-13 23:52:14 +0530

    updated sideNavBar

--- client/src/components/sidenav/SideNavbar.js
+++ client/src/components/sidenav/SideNavbar.js
@@ -3,33 +3,19 @@ import './SideNavbar.css'
 export const SideNavbar = () => {
   return (
     <nav id="navbar">
-          <a class="nav-link" href="#Introduction">Introduction</a>
+          <a className="nav-link">Home</a>
 
-          <a class="nav-link" href="#What_you_should_already_know">What you should already know
-</a>
+          <a className="nav-link">LogIn</a>
        
-          <a class="nav-link" href="#JavaScript_and_Java">JavaScript and Java
-</a>
+          <a className="nav-link">SignUp</a>
         
-          <a class="nav-link" href="#Hello_world">Hello world</a>
+          <a className="nav-link" >Playlist</a>
         
-          <a class="nav-link" href="#Variables">Variables</a>
+          <a className="nav-link">Favourites</a>
         
-          <a class="nav-link" href="#Declaring_variables">Declaring variables</a>
+          <a className="nav-link">Chat</a>
         
-          <a class="nav-link" href="#Variable_scope">Variable scope</a>
-        
-          <a class="nav-link" href="#Global_variables">Global variables
-</a>
-        
-          <a class="nav-link" href="#Constants">Constants</a>
-        
-          <a class="nav-link" href="#Data_types">Data types
-</a>
-        
-          <a class="nav-link" href="#if...else_statement">if...else statement</a>
-        
-          <a class="nav-link" href="#while_statement">while statement</a>
+          <a className="nav-link">Find a Friend</a>
       </nav>
   )
 }
