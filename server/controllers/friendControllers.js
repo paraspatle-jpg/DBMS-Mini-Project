bb33c19340c8eb5e64fe187b46ebe6508fc02158
paraspatle-jpg <paras18224827@gmail.com>
2022-10-18 23:57:21 +0530

    added friend controllers

---
+++ server/controllers/friendControllers.js
@@ -0,0 +1,31 @@
+import pool from "./../index.js";
+
+// onclick of add friend button this will be triggred
+export const addFriend = async (req, res) => {
+  try {
+    const user = req.user;
+  } catch (err) {}
+};
+
+//on page load this will send list of friends
+export const getFriends = async (req, res) => {
+  try {
+    const values = [req.user.user_id];
+    const friends = await pool.query(
+      "SELECT * FROM user_info WHERE user_id IN (SELECT friend_id FROM friends WHERE user_id=$1)",
+      values
+    );
+    friends.rows.map((ele) => {
+      delete ele.password;
+      return ele;
+    });
+
+    return res.status(200).send({ friends });
+  } catch (err) {}
+};
+
+// get a list of suggestions for friends
+export const getFriendSuggestions = async (req, res) => {
+  try {
+  } catch (err) {}
+};
