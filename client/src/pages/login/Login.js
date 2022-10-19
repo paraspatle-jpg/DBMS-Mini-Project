b828cf985bbd4f3027aa05f2dc91dd688303e944
paraspatle-jpg <paras18224827@gmail.com>
2022-10-19 23:14:54 +0530

    added login functionality

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React,{ useState} from "react";
 import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 import "./login.css";
 
-export const Login = () => {
+export const Login = ({ auth, setAuth}) => {
+  const [user, setUser] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUser((prevState) => {
+      console.log(user);
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+  const body =user;
+
+  const handleClick = async () =>{
+    try {
+      const res = await axios.post("http://localhost:5000/auth/login",body);
+      if(res.data.message === "Login Success")
+      {
+        console.log("Login Success")
+        localStorage.setItem("data",JSON.stringify(res.data));
+        setAuth(localStorage.getItem("data"));
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  if(auth){
+    return <Navigate to="/" replace={true} />
+  }
+
   return (
     <div className="main">
       <label htmlFor="chk" className="white ">
@@ -12,18 +46,21 @@ export const Login = () => {
         className="main-elements"
         name="email"
         placeholder="Email"
-        required=""
+        onChange={handleChange}
+        required="true"
       />
       <input
         className="main-elements"
         name="password"
         placeholder="Password"
-        required=""
+        onChange={handleChange}
+        required="true"
       />
       <button
         className="main-elements submit-button"
         id="login"
         type="button"
+        onClick={handleClick}
       >
         Login
       </button>
