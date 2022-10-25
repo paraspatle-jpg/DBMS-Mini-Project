b59b87cea702246da9cf90d210f529d1bcc98b7f
Tanmayee3012 <tanupgosavi30@gmail.com>
2022-10-26 02:18:43 +0530

    chat second push

--- client/src/pages/chat/Chat.js
+++ client/src/pages/chat/Chat.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./Chat.css";
 import { Link } from "react-router-dom";
-
+import FindFriend from "./../../assets/FindFriend.png";
 
 export const Chat = () => {
   return (
     <div>
       <div className="navbar-chat">
         <div className="navbar-header-chat">
-          <div className="dp-chat-1"></div>
+          <div className="search-chat">
+          <img className="navIcons-chat" src={FindFriend} alt="home" />
+          </div>
         </div>
         <div className="nav-link-chat" to="#">Paras</div>
         <div className="nav-link-chat" to="#">Paras</div>
@@ -16,7 +18,7 @@ export const Chat = () => {
       </div>
       <div className="messages-chat">
         <div className="header-chat">
-          <div className="dp-chat-2"></div>
+          <div className="dp-chat"></div>
         </div>
         <div className="text-chat">
           <div className="type-chat"></div>
